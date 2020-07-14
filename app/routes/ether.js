f53f7eee410f0cbf89f619ad5f8eacf587c5fc2b
arbazsiddiqui <arbaz00@gmail.com>
2020-07-14 20:11:42 +0530

    use router

--- app/routes/ether.js
+++ app/routes/ether.js
@@ -1,32 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
 var {validate} = require('cryptocurrency-address-validator');
 
 const {fetchLedger} = require('../controllers/etherController');
 
-module.exports = function (app) {
-	app.get('/getDetails', async (req, res) => {
-		try {
-			const {walletAddress} = req.query;
+router.get('/getDetails', async (req, res) => {
+	try {
+		const {walletAddress} = req.query;
 
-			if(!validate(walletAddress, 'ethereum')) {
-				return res.status(400).send({
-					message : "Invalid wallet address",
-					result : {}
-				})
-			}
-
-			const {withdrawals, deposits} = await fetchLedger(walletAddress);
-
-			return res.status(200).send({
-				message : "OK",
-				result : {
-					walletAddress,
-					withdrawals,
-					deposits
-				}
+		if (!validate(walletAddress, 'ethereum')) {
+			return res.status(400).send({
+				message: "Invalid wallet address",
+				result: {}
 			})
-		} catch (err) {
-			console.log(err);
-			return res.status(500).send({message: "Something went wrong"})
 		}
-	});
-};
+
+		const {withdrawals, deposits} = await fetchLedger(walletAddress);
+
+		return res.status(200).send({
+			message: "OK",
+			result: {
+				walletAddress,
+				withdrawals,
+				deposits
+			}
+		})
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send({message: "Something went wrong"})
+	}
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const env = process.env.NODE_ENV || "production";
 global.config = require('./config')[env];
 const port = process.env.PORT || config.port;
+const ether = require('./app/routes/ether');
 
-require('./app/routes/ether')(app);
+app.use('/api/v1', ether);
 app.listen(port);
 console.log("App listening on port " + port);
